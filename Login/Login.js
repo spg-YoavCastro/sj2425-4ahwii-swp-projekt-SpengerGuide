2bb455d172f69dfb5df9a5726f18428ba6644795
edvingrd <Gra22087@spengergasse.at>
2025-03-05 14:14:48 +0100

    Login fertig

--- Login/Login.js
+++ Login/Login.js
@@ -3,3 +3,14 @@ function Reg()
     window.location.href = "../Signup/signup.html";
 
 }
+function pwzeigen() {
+    let pw = document.getElementById("password");
+
+
+    if (pw.type === "text") {
+        pw.type = "password";
+        
+    } else {
+        pw.type = "text";
+    }
+}
