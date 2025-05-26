99b0fb9c2dd57a5c3ee655a891238a9c61c7a7ea
Luke4910 <Jan210325@spengergasse.at>
2025-05-27 00:22:38 +0200

    session fertig

--- Login/session_status.php
+++ Login/session_status.php
@@ -2,17 +2,14 @@
 session_start();
 header('Content-Type: application/json');
 
-$response = [
+// Debug-Ausgabe
+error_log("Session check - User: " . ($_SESSION['user_name'] ?? 'none'));
+
+echo json_encode([
     'loggedIn' => isset($_SESSION['user_id']),
-    'user' => null
-];
-
-if (isset($_SESSION['user_id'])) {
-    $response['user'] = [
-        'name' => $_SESSION['user_name'] ?? 'Benutzer',
+    'user' => [
+        'name' => $_SESSION['user_name'] ?? '',
         'email' => $_SESSION['user_email'] ?? ''
-    ];
-}
-
-echo json_encode($response);
+    ]
+]);
 ?>
